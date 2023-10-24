6ed64c4f6ea058bf79dafe9abb7231dbc9ea9923
LancersBucket <54820766+LancersBucket@users.noreply.github.com>
2023-10-23 20:03:24 -0400

    dynamic import cleanup and minor fixes

--- carbon.py
+++ carbon.py
@@ -2,35 +2,24 @@ import math, json
 import sys
 import dearpygui.dearpygui as dpg
 from pygame import mixer
-#import soundboard
 import importlib
 
 file = open("carbonConfig.json")
 config = json.load(file)
 
 dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=700, height=600)
+dpg.create_viewport(title='Carbon', width=700, height=600)
 dpg.setup_dearpygui()
-
-import imp
-import sys
 
 # function to dynamically load module
 def dynamic_module_import(module_name):
-   # find_module() is used to find the module in current directory
-   # it gets the pointer, path and description of the module
    try:
-      file_pointer, file_path, description = imp.find_module(module_name)
-   except ImportError:
-      print("Imported module {} not found".format(module_name))
-   try:
-      # load_module dynamically loads the module
-      # the parameters are pointer, path and description of the module 
-      load_module = imp.load_module(module_name, file_pointer, file_path, description)
+      load_module = importlib.import_module(module_name)
+      
+      # Run module initilization
       load_module.init()
    except Exception as e:
       print(e)
-   #return load_module
 
 for module in config["modules"]:
    dynamic_module_import(module)
